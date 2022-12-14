73c1fd4fa5fe6fa88432d4d421c5c398dd1a7132
Glen Pike <github@glenpike.co.uk>
2022-12-14 18:49:27 +0000

    Moving text around, etc.

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -12,6 +12,7 @@ export default async function (req, res) {
     temperature: 0.6,
     max_tokens: 500,
   });
+  // console.log(completion.data);
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
