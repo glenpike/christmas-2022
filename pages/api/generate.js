1ea094ba9fe5c6e3bfdef88ef44127bc4171993b
Glen Pike <github@glenpike.co.uk>
2022-12-17 21:37:23 +0000

    Add Poem type and style tweaks

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: generatePrompt(req.body.name),
+    prompt: generatePrompt(req.body.name, req.body.type),
     temperature: 0.6,
     max_tokens: 500,
   });
@@ -16,8 +16,11 @@ export default async function (req, res) {
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
-function generatePrompt(name) {
+function generatePrompt(name, type) {
   const capitalizedName =
     name[0].toUpperCase() + name.slice(1).toLowerCase();
+  if(type === 'poem') {
+    return `Write a short Christmas poem featuring ${capitalizedName}`
+  }
   return `Make a Christmas limerick about someone called ${capitalizedName}`;
 }
